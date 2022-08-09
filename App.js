7f6d207476d1487d7589770e15b52ca5170a80e5
1bananasplit <tboc300@gmail.com>
2022-08-09 01:05:23 +0000

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Switch } from 'react-native';
 
 export default function App() {
+
+const [value, setValue] = useState(false)
+const [text, setText] = useState('')
+
+const onChange = () => 
+setValue(previous => !previous);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+      </View>
+      <Text value={text} style={styles.mout}></Text>
+
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        ios_backgroundColor="#3e3e3e"
+        value={value}
+        onValueChange={onChange}
+      />
+      
     </View>
   );
 }
@@ -17,4 +36,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mout :{
+    margin: 15,
+  },
+  input:{
+    backgroundColor: "red",
+    borderColor: "black"
+  }
 });
